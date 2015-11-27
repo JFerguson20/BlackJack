79b9b276393232ca39d5c68c0d04cc03c750a3bc
James Ferguson <jakeferguson7@gmail.com>
2015-11-26 20:07:25 -0500

    init commit

---
+++ BlackJack/Deck.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+
+namespace BlackJack
+{
+    class Deck
+    {
+        List<int> cardsLeft; 
+
+        public Deck(int numDecks)
+        {
+            cardsLeft = new List<int>();
+            createCards(numDecks); 
+        }
+
+        private void createCards(int numDecks)
+        {
+            for(int i = 0; i < numDecks; i++)
+            {   
+                //add cards 1-9 (ace through 9)
+                for(int card = 1; card <10; card++)
+                {
+                    //each suit
+                    for(int j = 0; j < 4; j++)
+                    {
+                        cardsLeft.Add(card);
+                    }
+                }
+                //add face cards
+                for(int f = 0; f < 4; f++)
+                {
+                   //each suit
+                   for(int k =0; k < 4; k++)
+                    {
+                        cardsLeft.Add(10);
+                    }
+                }
+            }
+        }
+
+        public void shuffleCards()
+        {
+            int n = cardsLeft.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = ThreadSafeRandom.ThisThreadsRandom.Next(n + 1);
+                int value = cardsLeft[k];
+                cardsLeft[k] = cardsLeft[n];
+                cardsLeft[n] = value;
+            }
+        }
+
+        public int getCard()
+        {
+            var ret = cardsLeft[0];
+            cardsLeft.RemoveAt(0);
+            return ret;
+        }
+    }
+
+    public static class ThreadSafeRandom
+    {
+        [ThreadStatic]
+        private static Random Local;
+
+        public static Random ThisThreadsRandom
+        {
+            get { return Local ?? (Local = new Random(unchecked(Environment.TickCount * 31 + Thread.CurrentThread.ManagedThreadId))); }
+        }
+    }
+}
---
+++ BlackJack/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJack
+{
+    class Program
+    {
+        private static Deck deck;
+        static void Main(string[] args)
+        {
+
+            deck = new Deck(1);
+            deck.shuffleCards();
+
+        }
+
+        // return 1 for player won and -1 for dealer one
+        static int playHand()
+        {
+            bool canSplit = false;
+            var playersVal = 0;
+            var dealerVal = 0;
+
+            //get initial players and dealers card
+            var card = deck.getCard();
+            //player first card
+            if(card == 1)
+            {
+                playersVal = 11;
+            }
+            else
+            {
+                playersVal = card;
+            }
+            //get second card
+            card = deck.getCard();
+            if (card == playersVal)
+                canSplit = true;
+
+            if(card == 1)
+            {
+                if (playersVal >= 11)
+                    playersVal += 1;
+                else
+                    playersVal += 11;
+            }
+            else
+            {
+                playersVal += card;
+            }
+
+            //get dealer card
+            dealerVal = deck.getCard();
+            
+            //Now make the decisions
+
+            return 1;
+        }
+    }
+}
